590a13d14cee4d81402836126aab1036fdc4f1f5
Bookvino <94452945+Bookvino@users.noreply.github.com>
2025-04-14 00:35:18 +0200

    Update to allow origins

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # 🔐 CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://testlegion.com", "http://localhost:8000"],
+    allow_origins=["https://testlegion.com", "http://localhost:8000", "http://127.0.0.1:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
